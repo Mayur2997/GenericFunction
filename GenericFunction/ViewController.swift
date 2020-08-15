eb39341170311ad1193f77099b5c2c812c630b3c
Mayur Parmar <mrparmar2997@gmail.com>
2020-08-15 10:35:37 +0530

    - Initial Commit

--- GenericFunction/ViewController.swift
+++ GenericFunction/ViewController.swift
@@ -12,7 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Return value is Any
+        print("Age: \(AnyTypeGenericMethod(from: 22))")
+        print("Name: \(AnyTypeGenericMethod(from: "Mayur"))")
+
+
+        //Return value is dictionary
+        print("City: \(dictGenericMethod(from: [1: "Ahmedabad", 2: "Surat", 3: "Vadodra"]))") // [Int : String] type dictionary
+        print("Country Code: \(dictGenericMethod(from: ["India": 91 , "Singapore": 65, "USA": 1]))") // [String : Int] type dictionary
+        print("User: \(dictGenericMethod(from: ["Name": "Mayur", "Age": 22]))") // [String : Any] type dictionary
+ 
+        print("Addition: \(genericAddition(val1: 2, val2: 2.5))") // Add Int value and Double value
+    }
+    
+    
+    //Parameter is Any  -> Return value is Any
+    func AnyTypeGenericMethod<Value>(from dictionary:  Value) ->  Value {
+        return dictionary
+    }
+     
+    //Parameter is dictionary  -> Return value is dictionary
+    func dictGenericMethod<Key, Value>(from dictionary: [Key: Value]) -> [(Key, Value)] {
+        return Array(dictionary)
+    }
+
+    //Generics Addition
+    func genericAddition<T : Numeric >(val1: T, val2: T) -> T {
+        return val1+val2
     }
 
 
