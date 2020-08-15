4491128cfb44c14f6539653ec27e52e4eb2949c9
Mayur Parmar <mrparmar2997@gmail.com>
2020-08-15 10:33:55 +0530

    Initial Commit

---
+++ GenericFunction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenericFunction
+//
+//  Created by Mayur Parmar on 15/08/20.
+//  Copyright © 2020 Mayur Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
